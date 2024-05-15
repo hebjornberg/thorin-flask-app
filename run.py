917fb8007b79ebeba5e2bcfc5af736cf8decda03
Helena Bjornberg <helena.bjornberg@hotmail.com>
2024-05-15 19:29:35 +0000

    Second commit

--- run.py
+++ run.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index(): 
-    return "Hello World!"
+    return redner_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(
