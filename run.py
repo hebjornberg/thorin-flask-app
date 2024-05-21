65bd947abaf0e23b5ba0839bdda020c48beef9af
Helena Bjornberg <helena.bjornberg@hotmail.com>
2024-05-21 16:16:51 +0000

    Add changes to Flask project

--- run.py
+++ run.py
@@ -1,23 +1,49 @@
-import os 
-from flask import Flask
+import os
+import json
+from flask import Flask, render_template, request, flash
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def index(): 
-    return redner_template("index.html")
+def index():
+    return render_template("index.html")
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name): 
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data: 
+            if obj["url"] == member_name: 
+                member_obj
+    return render_template("member.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
+    return render_template("contact.html", page_title="Contact")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"), 
-        port=int(os.environ.get("PORT", "5000")), 
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
